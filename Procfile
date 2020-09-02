c64fdc5a334029507bfebee44a1672cde24aee89
Ubuntu <ubuntu@ip-172-31-55-96.ec2.internal>
2020-09-02 17:15:25 +0000

    Basic flask app created

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python mongo.py
+web: python app.py
---
+++ app.py
@@ -0,0 +1,56 @@
+import pymongo
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
+
+
+
+
+
+
+
+#MONGODB_URI = os.getenv("MONGO_URI")
+#DBS_NAME = "book_review_site"
+#COLLECTION_NAME = "books"
+
+#def mongo_connect(url):
+#    try:
+#        conn = pymongo.MongoClient(url)
+#        print("Mongo is connected!")
+#        return conn
+#    except pymongo.errors.ConnectionFailure as e:
+#        print("Could not connect to MongoDB %s") % e
+
+#conn = mongo_connect(MONGODB_URI)
+
+#coll = conn[DBS_NAME][COLLECTION_NAME]
+
+#documents = coll.find()
+
+#for doc in documents:
+#    print(doc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
