1251e54c4aae63630ac2c5fdbb3bddafc82792ab
Ubuntu <ubuntu@ip-172-31-55-96.ec2.internal>
2020-09-02 18:28:00 +0000

    book html template created

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import pymongo
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGODB_NAME"] = 'book_review_site'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_books')
+def get_books():
+    return render_template("books.html", books=mongo.db.books.find())
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
