be8aa851f7ecb2ae01a02b1f37df857a237ed3d6
Ubuntu <ubuntu@ip-172-31-55-96.ec2.internal>
2020-09-05 10:25:48 +0000

    requirements.txt file update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-import PyMongo
+import pymongo
 from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
