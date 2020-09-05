8105bb70f7c3dead2b02f2c5e1a4fab806382123
Ubuntu <ubuntu@ip-172-31-55-96.ec2.internal>
2020-09-05 10:39:28 +0000

    app.py update

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 import os
-import pymongo
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'book_review_site'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+brs_user = os.environ.get('BRS_USER')
+brs_password = os.environ.get('BRS_PASSWORD')
+
+app.config["MONGODB_NAME"] = 'book_review_site'
+app.config["MONGO_URI"] = 'mongodb+srv://brs_user:brs_password@bookreviewcluster.k2d12.mongodb.net/book_review_site?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+
+
+
+
+
 
 #@app.route('/')
 #@app.route('/get_books')
 #def get_books():
 #    return render_template("books.html", books=mongo.db.books.find())
+
+
+
+#if __name__ == '__main__':
+#    app.run(host=os.environ.get('IP'),
+#        port=int(os.environ.get('PORT')),
+#        debug=True)
+
 
 
 
