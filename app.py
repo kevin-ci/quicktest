b103b11905cb25a0a0c971138f9691de4f9a7e53
Ubuntu <ubuntu@ip-172-31-55-96.ec2.internal>
2020-09-05 10:11:49 +0000

    app.py update

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import os
-import pymongo
+import PyMongo
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'book_review_site'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
-
-
-
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
